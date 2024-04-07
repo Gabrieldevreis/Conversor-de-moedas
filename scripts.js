88ad333c7e0ace3fab08070130787335aa573915
Gabrieldevreis <gabrielreisdev@gmail.com>
2024-04-07 12:26:47 -0300

    redeclaração-de-variavel

--- scripts.js
+++ scripts.js
@@ -1,42 +1,38 @@
-const convertButton = document.querySelector(".convert-button")
-const escolherMoeda = document.querySelector(".change-currency").value
+const convertButton = document.querySelector(".convert-button");
 
-function convertValues(){
-    const inputValue = document.querySelector(".input-value").value
-    const valorParaConverter = document.querySelector(".moeda-valor-converter")
-    const valorConvertido = document.querySelector(".moeda-valor-convertido")
-    
+function convertValues() {
+    const inputValue = document.querySelector(".input-value").value;
+    const escolherMoeda = document.querySelector(".change-currency").value;
+    const valorParaConverter = document.querySelector(".moeda-valor-converter");
+    const valorConvertido = document.querySelector(".moeda-valor-convertido");
 
-    const dolarDay = 5.2
-    const euroDay = 6.3
+    const dolarDay = 5.2;
+    const euroDay = 6.3;
 
+    valorParaConverter.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputValue);
 
-    valorParaConverter.innerHTML = new Intl.NumberFormat("pt-BR",{
-        style:"currency",
-        currency:"BRL"
-    }).format(inputValue)
-
-    
-    if(escolherMoeda == "dolar"){
-        valorConvertido.innerHTML = new Intl.NumberFormat("en-US",{
-            style:"currency",
-            currency:"USD"
-        }).format(inputValue / dolarDay) 
+    if (escolherMoeda == "dolar") {
+        valorConvertido.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputValue / dolarDay);
     }
 
-    if(escolherMoeda == "euro"){
-        valorConvertido.innerHTML = new Intl.NumberFormat("de-DE",{
-            style:"currency",
-            currency:"EUR"
-        }).format(inputValue / euroDay) 
+    if (escolherMoeda == "euro") {
+        valorConvertido.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputValue / euroDay);
     }
-    
 }
 
-function mudarTexto(){
-    console.log("mudei")
+function mudarTexto() {
+    console.log("mudei");
 }
 
-escolherMoeda.addEventListener("change" , mudarTexto)
-convertButton.addEventListener("click" , convertValues)
+document.querySelector(".change-currency").addEventListener("change", mudarTexto);
+convertButton.addEventListener("click", convertValues);
 
